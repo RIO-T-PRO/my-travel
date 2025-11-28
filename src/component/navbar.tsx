7c408c36f63a83d98a07cf4f735f1125b63443c7
RIO-T-PRO <malibaalex42@gmail.com>
2025-11-28 20:18:10 +0300

    I'v worked on the navBar

--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -1,10 +1,25 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const pathname = usePathname();
+
+  const navLinks = [
+    { label: "Home", href: "/" },
+    { label: "Flights", href: "/flights" },
+    { label: "Resorts", href: "/resorts" },
+    { label: "Resources", href: "/resources" },
+    { label: "About", href: "/about" },
+    { label: "Contact", href: "/contact" },
+  ];
+
+  const handleActiveLink = (link: string) => {
+    return pathname === link;
+  };
 
   return (
     <header className="bg-background-light dark:bg-card-dark/80 backdrop-blur-sm sticky top-0 z-40 border-b border-slate-200 dark:border-slate-800">
@@ -18,42 +33,22 @@ const NavBar = () => {
           </Link>
 
           <nav className="hidden md:flex items-center space-x-8">
-            <Link
-              href="/"
-              className="text-slate-600 dark:text-slate-300 hover:text-primary dark:hover:text-primary transition-colors"
-            >
-              Home
-            </Link>
-            <Link
-              href="/flights"
-              className="text-slate-600 dark:text-slate-300 hover:text-primary dark:hover:text-primary transition-colors"
-            >
-              Flights
-            </Link>
-            <Link
-              href="/resorts"
-              className="text-slate-600 dark:text-slate-300 hover:text-primary dark:hover:text-primary transition-colors"
-            >
-              Resorts
-            </Link>
-            <Link
-              href="/resources"
-              className="text-slate-600 dark:text-slate-300 hover:text-primary dark:hover:text-primary transition-colors"
-            >
-              Resources
-            </Link>
-            <Link
-              href="/about"
-              className="text-slate-600 dark:text-slate-300 hover:text-primary dark:hover:text-primary transition-colors"
-            >
-              About
-            </Link>
-            <Link
-              href="/contact"
-              className="text-slate-600 dark:text-slate-300 hover:text-primary dark:hover:text-primary transition-colors"
-            >
-              Contact
-            </Link>
+            {navLinks.map((link, index) => (
+              <Link
+                key={index}
+                href={link.href}
+                // onClick={() => setActiveLink(link.href)}
+                className={`
+                  ${
+                    handleActiveLink(link.href)
+                      ? "text-primary "
+                      : " text-slate-200 "
+                  }
+                  hover:text-primary dark:hover:text-primary transition-colors`}
+              >
+                {link.label}
+              </Link>
+            ))}
           </nav>
 
           <div className="flex items-center space-x-4">
@@ -73,42 +68,21 @@ const NavBar = () => {
 
         {isMenuOpen && (
           <nav className="md:hidden mt-4 pb-4 space-y-2">
-            <Link
-              href="/"
-              className="block py-2 text-slate-600 dark:text-slate-300 hover:text-primary"
-            >
-              Home
-            </Link>
-            <Link
-              href="/flights"
-              className="block py-2 text-slate-600 dark:text-slate-300 hover:text-primary"
-            >
-              Flights
-            </Link>
-            <Link
-              href="/resorts"
-              className="block py-2 text-slate-600 dark:text-slate-300 hover:text-primary"
-            >
-              Resorts
-            </Link>
-            <Link
-              href="/resources"
-              className="block py-2 text-slate-600 dark:text-slate-300 hover:text-primary"
-            >
-              Resources
-            </Link>
-            <Link
-              href="/about"
-              className="block py-2 text-slate-600 dark:text-slate-300 hover:text-primary"
-            >
-              About
-            </Link>
-            <Link
-              href="/contact"
-              className="block py-2 text-slate-600 dark:text-slate-300 hover:text-primary"
-            >
-              Contact
-            </Link>
+            {navLinks.map((link, index) => (
+              <Link
+                key={index}
+                href={link.href}
+                className={`
+                    block py-2 hover:text-primary
+                    ${
+                      handleActiveLink(link.href)
+                        ? "text-primary font-semibold"
+                        : "text-slate-600 dark:text-slate-300"
+                    }`}
+              >
+                {link.label}
+              </Link>
+            ))}
           </nav>
         )}
       </div>
